d39e4f43b320f55894d3ecfb1adf91907087eb3b
DerWikinger <derwikinger@yandex.ru>
2020-07-05 00:52:55 +0300

    В класс OPCServer добавлены свойства clsid и clsidcat

--- OPCClientLibrary/OPCServer.cpp
+++ OPCClientLibrary/OPCServer.cpp
@@ -18,3 +18,25 @@ const string OPCServer::name() {
 const string OPCServer::name(const string& value) {
 	return this->_name = value;
 }
+
+const CLSID OPCServer::clsid() {
+	return this->_clsid;
+}
+
+const CLSID OPCServer::clsid(const CLSID& value) {
+	return this->_clsid = value;
+}
+
+const CLSID OPCServer::clsidcat() {
+	return this->_clsidcat;
+}
+
+const CLSID OPCServer::clsidcat(const CLSID& value) {
+	return this->_clsidcat = value;
+}
+
+const string OPCServer::ToString() {
+	return "OPCServerObject";
+	return this->_name;
+}
+
--- OPCClientLibrary/OPCServer.h
+++ OPCClientLibrary/OPCServer.h
@@ -5,7 +5,6 @@
 namespace OPCServerLibrary {
 
 	using namespace std;
-
 	using namespace OpcEnumLib;
 	using namespace OPCDA;
 
@@ -16,17 +15,25 @@ namespace OPCServerLibrary {
 		IUnknown* srv;
 		IOPCServer* server;
 		string _name;
+		CLSID _clsid;
+		CLSID _clsidcat;
 	public:
 		OPCServer() : _name("") {}
 		explicit OPCServer(const string& name) : _name(name) {}
+		
 		const string name();
 		const string name(const string& value);
+		const CLSID clsid();
+		const CLSID clsid(const CLSID& value);
+		const CLSID clsidcat();
+		const CLSID clsidcat(const CLSID& value);
 		virtual ~OPCServer() {
 			delete srv;
 			delete server;
 		}
 		void Connect();
 		void Disconnect();
+		const string ToString();
 	};
 }
 
